b3d7afb2272eeebe39fe972f22c7059fb888f4b1
Phinn Sriployrung <psrip001@gmail.com>
2021-05-20 13:27:25 -0700

    added client origin

--- src/app.js
+++ src/app.js
@@ -15,7 +15,11 @@ const app = express();
 const morganOption = (NODE_ENV === 'production') ? 'tiny' : 'common';
 
 app.use(morgan(morganOption));
-app.use(cors());
+app.use(
+    cors({
+        origin: CLIENT_ORIGIN,
+    })
+);
 app.use(helmet());
 
 app.use('/api/auth', authRouter)
--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 module.exports = {
-    PORT:  || 8000,
+    PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || "development",
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://PhinnS@localhost/rose-gardn',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
     JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
+    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || "http://localhost:3000",
 };
