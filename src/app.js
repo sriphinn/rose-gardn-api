703c329491d8c2bdee9c7251d1c9ae18e9fbf42c
Phinn Sriployrung <psrip001@gmail.com>
2021-05-20 13:55:12 -0700

    removed client-origin

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
-const { NODE_ENV, CLIENT_ORIGIN } = require('./config');
+const { NODE_ENV } = require('./config');
 const logger = require('./logger');
 const authRouter = require('./auth/auth-router');
 const usersRouter = require('./users/users-router');
@@ -15,11 +15,7 @@ const app = express();
 const morganOption = (NODE_ENV === 'production') ? 'tiny' : 'common';
 
 app.use(morgan(morganOption));
-app.use(
-    cors({
-        origin: CLIENT_ORIGIN,
-    })
-);
+app.use(cors());
 app.use(helmet());
 
 app.use('/api/auth', authRouter)
--- src/config.js
+++ src/config.js
@@ -4,5 +4,4 @@ module.exports = {
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://PhinnS@localhost/rose-gardn',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
     JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
-    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || "http://localhost:3000",
 };
